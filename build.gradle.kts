84dbf163f2ae8980a6975de350c0e91287fb8cbe
mfrancza <mfrancza@gmail.com>
2023-01-11 21:22:52 -0600

    Move RuleSet Client to a Multiplatform Library
    Before:
    -The RuleSet Client was defined in the package for the rule manager
    -The RuleSet Client tests were not platform independent due to using java.time
    
    After:
    -This package contains the RuleSet client
    -The tests use a constant epoch seconds reference instead

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+val ktorVersion = "2.1.3"
+val coroutinesVersion = "1.6.4"
+
+plugins {
+    kotlin("multiplatform") version "1.8.0"
+    id("maven-publish")
+}
+
+group = "com.mfrancza"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js(IR) {
+        browser {
+            testTask {
+                useKarma {
+                    useFirefox()
+                }
+            }
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("com.mfrancza:jwt-revocation-rules:1.0-SNAPSHOT")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("io.ktor:ktor-client-mock:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
