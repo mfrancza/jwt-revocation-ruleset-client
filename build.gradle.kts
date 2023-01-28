e72b8f8aafb7429c9884e8773b6142d590675f1b
mfrancza <mfrancza@gmail.com>
2023-01-28 15:04:24 -0600

    Update Karma for CVE-2022-25927

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,3 +67,12 @@ kotlin {
         val nativeTest by getting
     }
 }
+
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
+        lockFileDirectory = project.rootDir.resolve("kotlin-js-store")
+    }
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().apply {
+        versions.karma.version = "6.4.1"
+    }
+}
